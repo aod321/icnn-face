4901ebcb0a4ecf99d80385919792c6d1eeb49163
yinzi <Myinzi123@gmail.com>
2019-07-08 17:20:07 +0800

    second

--- model.py
+++ model.py
@@ -4,40 +4,66 @@ import torch.nn.functional as F
 
 
 class iCNNnode(torch.nn.Module):
-    def __init__(self,input,pre,post):
+    def __init__(self):
 
         super(iCNNnode,self).__init__()
 
-        self.pre =  pre
-        self.post = post
-        self.x =  input
 
         self.in_channels = 3
         self.out_channels = 32
         self.kernel_size = 3
-        self.stride = 1
+        self.stride = self.kernel_size//2
         self.padding = 1
 
+        self.interpol_size = 3
+        self.interpol_mode = 'nearest'
 
-        self.y = nn.Conv2d(self.x,in_channels=self.in_channels,out_channels=self.out_channels,kernel_size=self.kernel_size,stride=self.stride,padding=self.padding)
-        self.y = self.pre + y + self.post
-        self.pre =
-        self.post =
+        self.pool_size = 3
+        self.pool_stride = self.pool_size//2
 
-
-
+        self.conv_node = nn.Conv2d(in_channels=self.in_channels,out_channels=self.out_channels,kernel_size=self.kernel_size,stride=self.stride,padding=self.padding)
 
     def set_Conv(self,in_channels,out_channels,kernel_size,stride,padding):
 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
     def set_upsample(self, interpol_size, interpol_mode):
+        self.interpol_size = interpol_size
+        self.interpol_mode = interpol_mode
+
 
     def set_downsample(self, pool_size, pool_stride):
 
+        self.pool_size = pool_size
+        self.pool_stride = pool_stride
+
+
     def upsample(self, input):
+        result = F.interpolate(input, scale_factor=self.interpol_size,mode=self.interpol_mode)
+        return result
 
     def downsample(self, input):
+        result = F.max_pool2d(input,kernel_size=self.pool_size,stride=self.pool_stride)
+        return result
 
-    def forward(self,x):
+    def forward(self,x,pre,post):
+
+        y = self.conv_node(x)
+        self.pre =  self.upsample(y)
+        self.post = self.downsample(y)
+        y = pre + self.y + post
+
+        return y,self.pre,self.post
+
+class iCNN_Cell(torch.nn.Module):
+
+    def __init__(self):
+        super(iCNN_Cell,self).__init__()
+        iCNN_Nodelist =
 
 
 
