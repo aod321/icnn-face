1774cd4529f09f4ffc4e128cd96274465627c7d2
yinzi <Myinzi123@gmail.com>
2019-07-26 22:13:15 +0800

    change train_stage2

--- train_stage2.py
+++ train_stage2.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 import torch.optim as optim
 from model_1 import FaceModel
 # from es_model import ICNN
-from torch.utils.data import DataLoader
-from dataset import HelenDataset, FacialPartDataset, MouthDataset
+from torch.utils.data import DataLoader, ConcatDataset
+from dataset import HelenDataset, SinglePart
 from Helen_transform import Resize, ToPILImage, ToTensor, Normalize, RandomRotation, \
-                                RandomResizedCrop, CenterCrop, LabelsToOneHot, Stage2_ToTensor, Stage2_ToPILImage
+                                RandomResizedCrop, CenterCrop, LabelsToOneHot
 from torchvision import transforms
 import argparse
 import numpy as np
@@ -29,85 +29,84 @@ print(args)
 # Dataset Read_in Part
 # root_dir = "/data1/yinzi/facial_parts"
 root_dir = '/home/yinzi/data/facial_parts'
-
 txt_file_names = {
     'train': "exemplars.txt",
     'val': "tuning.txt"
 }
 
-
-face_transforms_list = {
+transforms_list = {
     'train':
         transforms.Compose([
-            Stage2_ToPILImage(),
-            # RandomRotation(15),
-            # RandomResizedCrop((64, 64), scale=(0.9, 1.1)),
-            # CenterCrop((512,512)),
-            # Resize((64, 64)),
             LabelsToOneHot(),
-            Stage2_ToTensor()
+            ToTensor()
         ]),
     'val':
         transforms.Compose([
-            Stage2_ToPILImage(),
-            # Resize((64, 64)),
             LabelsToOneHot(),
-            Stage2_ToTensor()
+            ToTensor()
         ])
 }
 
-mouth_transforms_list =  {
-    'train':
-        transforms.Compose([
-            Stage2_ToPILImage(),
-            # RandomRotation(15),
-            # RandomResizedCrop((64, 64), scale=(0.9, 1.1)),
-            # CenterCrop((512,512)),
-            # Resize((80, 80)),
-            LabelsToOneHot(),
-            Stage2_ToTensor()
-        ]),
-    'val':
-        transforms.Compose([
-            Stage2_ToPILImage(),
-            # Resize((64, 64)),
-            LabelsToOneHot(),
-            Stage2_ToTensor()
-        ])
-}
-
-transforms_list = {'face':face_transforms_list,
-                   'mouth':mouth_transforms_list
-                   }
-
-Facialpart_dataset = {x: FacialPartDataset(txt_file=txt_file_names[x],
+# _____________________________________
+eyebrow1_dataset = {x: SinglePart(txt_file=txt_file_names[x],
                                            root_dir=root_dir,
-                                           transform=transforms_list['face'][x]
-                                  )
+                                           transform=transforms_list[x]
+                                  ).set_part(name='eyebrow1',range=range(2,3),label_numbers=1)
                       for x in ['train', 'val']}
+eyebrow2_dataset = {x: SinglePart(txt_file=txt_file_names[x],
+                                           root_dir=root_dir,
+                                           transform=transforms_list[x]
+                                  ).set_part(name='eyebrow2',range=range(3,4),label_numbers=1)
+                      for x in ['train', 'val']}
+eye1_dataset = {x: SinglePart(txt_file=txt_file_names[x],
+                              root_dir=root_dir,
+                              transform=transforms_list[x]
+                              ).set_part(name='eye1', range=range(4, 5), label_numbers=1)
+                      for x in ['train', 'val']}
+eye2_dataset = {x: SinglePart(txt_file=txt_file_names[x],
+                                           root_dir=root_dir,
+                                           transform=transforms_list[x]
+                                  ).set_part(name='eye2',range=range(5,6),label_numbers=1)
+                      for x in ['train', 'val']}
+nose_dataset = {x: SinglePart(txt_file=txt_file_names[x],
+                                           root_dir = root_dir,
+                                           transform = transforms_list[x]
+                                  ).set_part(name='nose', range=range(6,7),label_numbers=1)
+                      for x in ['train', 'val']}
+mouth_dataset = {x: SinglePart(txt_file=txt_file_names[x],
+                                           root_dir = root_dir,
+                                           transform = transforms_list[x]
+                                  ).set_part(name='mouth', range=range(7,10),label_numbers=3)
+                      for x in ['train', 'val']}
+# _____________________________________
 
-Mouth_dataset = {x: MouthDataset(txt_file=txt_file_names[x],
-                                 root_dir=root_dir,
-                                 transform=transforms_list['mouth'][x]
-                                  )
-                 \
+eyes_dataset = {x: ConcatDataset([eye1_dataset[x], eye2_dataset[x]])
+                      for x in ['train', 'val']}
+eyebrows_dataset = {x: ConcatDataset([eyebrow1_dataset[x], eyebrow2_dataset[x]])
+                      for x in ['train', 'val']}
+# _____________________________________
 
-
-                 for x in ['train', 'val']}
-
-face_dataloaders = {x: DataLoader(Facialpart_dataset[x], batch_size=args.batch_size,
+eyes_train_loader = {x: DataLoader(eyes_dataset[x], batch_size=args.batch_size,
+                                  shuffle=True, num_workers=4)
+                    for x in ['train', 'val']}
+eyebrows_train_loader = {x: DataLoader(eyebrows_dataset[x], batch_size=args.batch_size,
                                   shuffle=True, num_workers=4)
                     for x in ['train', 'val']}
 
-mouth_dataloaders = {x: DataLoader(Mouth_dataset[x], batch_size=args.batch_size,
-                                   shuffle=True, num_workers=4)
-                     for x in ['train', 'val']}
+nose_train_loader = {x: DataLoader(nose_dataset[x], batch_size=args.batch_size,
+                                  shuffle=True, num_workers=4)
+                    for x in ['train', 'val']}
 
-dataloaders = {'face': face_dataloaders,
-               'mouth': mouth_dataloaders}
+mouth_train_loader = {x: DataLoader(mouth_dataset[x], batch_size=args.batch_size,
+                                  shuffle=True, num_workers=4)
+                    for x in ['train', 'val']}
 
-dataset1_sizes = {x: len(Facialpart_dataset[x]) for x in ['train', 'val']}
-dataset2_sizes = {x: len(Mouth_dataset[x]) for x in ['train', 'val']}
+model_name_list = ['eyebrows', 'eyes', 'nose', 'mouth']
+data_lodaers = {model_name_list[0]: eyebrows_train_loader,
+                 model_name_list[1]: eyes_train_loader,
+                 model_name_list[2]: nose_train_loader,
+                 model_name_list[3]: mouth_train_loader
+                 }
 
 
 class Stage2FaceModel(FaceModel):
@@ -137,9 +136,117 @@ class Stage2FaceModel(FaceModel):
         self.last_conv3_bnm = nn.BatchNorm2d(self.lable_channel_size)
 
 
-class TrainStage2(TemplateModel):
+class EyebrowTrain(TemplateModel):
     def __init__(self, argus=args):
-        super(TrainStage2, self).__init__()
+        super(EyebrowTrain, self).__init__()
+        self.label_channels = 2
+        # ============== not neccessary ===============
+        self.train_logger = None
+        self.eval_logger = None
+        self.args = argus
+
+        # ============== neccessary ===============
+        self.writer = SummaryWriter('log')
+        self.step = 0
+        self.epoch = 0
+        self.best_error = float('Inf')
+
+        self.device = torch.device("cuda:%d"%self.args.cuda if torch.cuda.is_available() else "cpu")
+
+        self.model = Stage2FaceModel().to(self.device)
+        self.model.set_label_channels(self.label_channels)
+        # self.optimizer = optim.SGD(self.model.parameters(), self.args.lr,  momentum=0.9, weight_decay=0.0)
+        self.optimizer = optim.Adam(self.model.parameters(), self.args.lr)
+        # self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.metric = nn.CrossEntropyLoss()
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.5)
+
+        self.train_loader = data_lodaers['eyebrows']['train']
+        self.eval_loader = data_lodaers['eyebrows']['val']
+
+        self.ckpt_dir = "checkpoints_eyebrow"
+        self.display_freq = args.display_freq
+
+        # call it to check all members have been intiated
+        self.check_init()
+
+
+class EyeTrain(TemplateModel):
+    def __init__(self, argus=args):
+        super(EyeTrain, self).__init__()
+        self.label_channels = 2
+        # ============== not neccessary ===============
+        self.train_logger = None
+        self.eval_logger = None
+        self.args = argus
+
+        # ============== neccessary ===============
+        self.writer = SummaryWriter('log')
+        self.step = 0
+        self.epoch = 0
+        self.best_error = float('Inf')
+
+        self.device = torch.device("cuda:%d"%self.args.cuda if torch.cuda.is_available() else "cpu")
+
+        self.model = Stage2FaceModel().to(self.device)
+        self.model.set_label_channels(self.label_channels)
+        # self.optimizer = optim.SGD(self.model.parameters(), self.args.lr,  momentum=0.9, weight_decay=0.0)
+        self.optimizer = optim.Adam(self.model.parameters(), self.args.lr)
+        # self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.metric = nn.CrossEntropyLoss()
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.5)
+
+        self.train_loader = data_lodaers['eyes']['train']
+        self.eval_loader = data_lodaers['eyes']['val']
+
+        self.ckpt_dir = "checkpoints_eyes"
+        self.display_freq = args.display_freq
+
+        # call it to check all members have been intiated
+        self.check_init()
+
+
+class NoseTrain(TemplateModel):
+    def __init__(self, argus=args):
+        super(NoseTrain, self).__init__()
+        self.label_channels = 2
+        # ============== not neccessary ===============
+        self.train_logger = None
+        self.eval_logger = None
+        self.args = argus
+
+        # ============== neccessary ===============
+        self.writer = SummaryWriter('log')
+        self.step = 0
+        self.epoch = 0
+        self.best_error = float('Inf')
+
+        self.device = torch.device("cuda:%d"%self.args.cuda if torch.cuda.is_available() else "cpu")
+
+        self.model = Stage2FaceModel().to(self.device)
+        self.model.set_label_channels(self.label_channels)
+        # self.optimizer = optim.SGD(self.model.parameters(), self.args.lr,  momentum=0.9, weight_decay=0.0)
+        self.optimizer = optim.Adam(self.model.parameters(), self.args.lr)
+        # self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.metric = nn.CrossEntropyLoss()
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.5)
+
+        self.train_loader = data_lodaers['nose']['train']
+        self.eval_loader = data_lodaers['nose']['val']
+
+        self.ckpt_dir = "checkpoints_nose"
+        self.display_freq = args.display_freq
+
+        # call it to check all members have been intiated
+        self.check_init()
+
+
+class MouthTrain(TemplateModel):
+    def __init__(self, argus=args):
+        super(MouthTrain, self).__init__()
         self.label_channels = 6
         # ============== not neccessary ===============
         self.train_logger = None
@@ -150,128 +257,41 @@ class TrainStage2(TemplateModel):
         self.writer = SummaryWriter('log')
         self.step = 0
         self.epoch = 0
-        self.best_error = {x: float('Inf')
-                           for x in ['face', 'mouth']}
+        self.best_error = float('Inf')
 
-        self.device = torch.device("cuda:%d" % self.args.cuda if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:%d"%self.args.cuda if torch.cuda.is_available() else "cpu")
 
-        self.model = {x: Stage2FaceModel().to(self.device)
-                      for x in ['face', 'mouth']}
-        self.model['face'].set_label_channels(5)
-        self.model['mouth'].set_label_channels(1)
-
+        self.model = Stage2FaceModel().to(self.device)
+        self.model.set_label_channels(self.label_channels)
         # self.optimizer = optim.SGD(self.model.parameters(), self.args.lr,  momentum=0.9, weight_decay=0.0)
-        self.optimizer = {x:optim.Adam(self.model[x].parameters(), self.args.lr)
-                          for x in ['face', 'mouth']}
-        self.criterion = {x: nn.BCEWithLogitsLoss()
-                          for x in ['face', 'mouth']}
+        self.optimizer = optim.Adam(self.model.parameters(), self.args.lr)
+        # self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.BCEWithLogitsLoss()
         self.metric = nn.CrossEntropyLoss()
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.5)
 
-        self.scheduler = {x: optim.lr_scheduler.StepLR(self.optimizer[x], step_size=15, gamma=0.5)
-                          for x in ['face', 'mouth']}
+        self.train_loader = data_lodaers['mouth']['train']
+        self.eval_loader = data_lodaers['mouth']['val']
 
-        self.train_loader = dataloaders
-        self.eval_loader = dataloaders
-        # self.mouth_train_loader = mouth_dataloaders['train']
-        # self.mouth_eval_loader = mouth_dataloaders['val']
-
-        self.ckpt_dir = {x: "checkpoints_%s" % x
-                         for x in ['face', 'mouth']}
+        self.ckpt_dir = "checkpoints_mouth"
         self.display_freq = args.display_freq
 
         # call it to check all members have been intiated
         self.check_init()
 
-        self.x = None
-
-    def train(self):
-        self.epoch += 1
-        self.model[self.x].train()
-        iters = 0
-        for batch in self.train_loader[self.x]['train']:
-                image, labels = batch['image'].to(self.device), batch['labels'].to(self.device)
-                self.step += 1
-                iters += 1
-                self.optimizer[self.x].zero_grad()
-
-                pred = self.model[self.x](image)
-                loss = self.criterion[self.x](pred, labels)
-
-                loss.backward()
-                self.optimizer[self.x].step()
-
-                if self.step % self.display_freq == 0:
-                    self.writer.add_scalar('loss_%s' % x, loss.item(), self.step)
-                    print('mode {}\tepoch {}\tstep {}\titers{}/{}\t loss {:.3}'.format(
-                             self.x, self.epoch, self.step, len(self.train_loader[self.x]), iters, loss.item())
-                          )
-
-
-
-    def check_init(self):
-        assert self.model
-        assert self.optimizer
-        assert self.criterion
-        assert self.metric
-        assert self.train_loader
-        assert self.eval_loader
-        assert self.device
-        assert self.ckpt_dir
-        assert self.display_freq
-        assert self.scheduler
-
-        for x in ['face', 'mouth']:
-            if not osp.exists(self.ckpt_dir[x]):
-                os.mkdir(self.ckpt_dir[x])
-
-
-    def eval(self):
-        self.model[self.x].eval()
-        error = {x: 0
-                 for x in ['face', 'mouth']}
-        loss_list = []
-        for batch in self.eval_loader[self.x]['val']:
-            image, labels = batch['image'].to(self.device), batch['labels'].to(self.device)
-            pred = self.model[self.x](image)
-            error = self.criterion[self.x](pred, labels)
-            loss_list.append(error.item())
-        error[self.x] = np.mean(loss_list)
-
-        if error[self.x] < self.best_error[self.x]:
-            self.best_error[self.x] = error[self.x]
-            self.save_state(osp.join(self.ckpt_dir[self.x], 'best.pth.tar'), False)
-        self.save_state(osp.join(self.ckpt_dir[self.x], '{}.pth.tar'.format(self.epoch)))
-        self.writer.add_scalar('error_%s' % self.x, error, self.epoch)
-        print('epoch {}\terror {:.3}'.format(self.epoch, error))
-
-        return error[self.x]
-
-    def save_state(self, fname, optim=True):
-        state = {}
-        if isinstance(self.model[self.x], torch.nn.DataParallel):
-            state['model'] = self.model[self.x].module.state_dict()
-        else:
-            state['model'] = self.model[self.x].state_dict()
-
-        if optim:
-            state['optimizer'] = self.optimizer[self.x].state_dict()
-        state['step'] = self.step
-        state['epoch'] = self.epoch
-        state['best_error'] = self.best_error[self.x]
-        torch.save(state, fname)
-        print('save model at {}'.format(fname))
-
 
 def start_train():
-    train = TrainStage2(args)
-    for x in ['face', 'mouth']:
-        train.x = x
+    train = {model_name_list[0]: EyebrowTrain(args),
+             model_name_list[1]: EyeTrain(args),
+             model_name_list[2]: NoseTrain(args),
+             model_name_list[3]: MouthTrain(args)
+    }
+    for x in model_name_list:
         for epoch in range(args.epochs):
-            train.scheduler[x].step()
-            train.train()
+            # train[x].scheduler.step()
+            train[x].train()
             if (epoch + 1) % args.eval_per_epoch == 0:
-                train.eval()
-
+                train[x].eval()
 
     print('Done!!!')
 
